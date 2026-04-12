dcdd144b8a8ea0681d48584b34b46fd5c534d1c4
KajalVasal <kajalvasal8@gmail.com>
2026-04-12 12:59:17 +1200

    Refactor power calculation and input handling

--- power.py
+++ power.py
@@ -1,7 +1,11 @@
 def calculate_power(x,y):
-  return(x**y)
-  x=int(input("enter the value of x: "))
-  y=int(input("enter the value of y:"))
+  return x**y
 
-print("result:",calculate_power(x,y))
+if_name_ =="main":
+  x=int(input("enter the base: "))
+  y=int(input("enter the exponent: "))
+  
+  result= calculate_power(x,y)
+  print(f"{x} raised to the power of {y} is:{result}")
+  
 
