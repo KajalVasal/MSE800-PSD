b6b5307fe7c5314ce795043add42dddf09e3c58f
KajalVasal <kajalvasal8@gmail.com>
2026-04-12 14:35:49 +1200

    Update power.py

--- power.py
+++ power.py
@@ -1,7 +1,2 @@
-def calculate_power(x,y):
-  return(x**y)
-  x=int(input("enter the value of x: "))
-  y=int(input("enter the value of y:"))
-
-print("result:",calculate_power(x,y))
+print("hello world")
 
